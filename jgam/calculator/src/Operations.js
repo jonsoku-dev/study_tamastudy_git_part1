6966a56441117c26545b1a524a13a7923f7fe63e
jgam <jgam@alumni.nd.edu>
2019-10-08 19:26:55 +0900

    done

--- jgam/calculator/src/Operations.js
+++ jgam/calculator/src/Operations.js
@@ -3,12 +3,28 @@ import React from 'react';
 const Operations = ({handleOperations}) => {
     return (
         <div>
-            <button onClick={() => handleOperations('+')}>+</button>
-            <button onClick={() => handleOperations('-')}>-</button>
-            <button onClick={() => handleOperations('*')}>*</button>
-            <button onClick={() => handleOperations('/')}>/</button>
-            <button onClick={() => handleOperations('ce')}>ce</button>
-            <button onClick={() => handleOperations('=')}>=</button>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+
+            <div className="arithmetic">
+            <button type="button" class="btn btn-secondary btn-lg" onClick={() => handleOperations('+')}>+</button>
+            <button type="button" class="btn btn-secondary btn-lg" onClick={() => handleOperations('-')}>-</button>
+            <button type="button" class="btn btn-secondary btn-lg" onClick={() => handleOperations('*')}>*</button>
+            <button type="button" class="btn btn-secondary btn-lg" onClick={() => handleOperations('/')}>/</button>
+            </div>
+
+            <div className="runFunction">
+            <button type="button" class="btn btn-primary btn-lg" onClick={() => handleOperations('ce')}>ce</button>
+            <button type="button" class="btn btn-primary btn-lg" onClick={() => handleOperations('=')}>=</button>
+            </div>
+
+
+
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
             <button onClick={() => handleOperations('(')}>(</button>
             <button onClick={() => handleOperations(')')}>)</button>
 
