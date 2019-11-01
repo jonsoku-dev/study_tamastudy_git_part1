cf20a459e2d09d82effc53b95581b4ae441f11d7
jgam <jgam@alumni.nd.edu>
2019-11-01 17:01:10 +0900

    [api] connection

--- jgam/react_calendar/src/sample01.js
+++ jgam/react_calendar/src/sample01.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Calendar, momentLocalizer, Views} from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { HolidayAPI } from 'holidayapi';
 
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment);
@@ -9,8 +10,49 @@ let allViews = Object.keys(Views).map(k=>Views[k])
 console.log(allViews);
 
 /*
-
+This is the API part
 */
+
+const key = '2ff13222-6dea-4574-b601-a508811fb28e';
+const holidayApi = new HolidayAPI({ key });
+
+console.log(holidayApi["countries"]);
+//holiday
+// Fetch supported countries and subdivisions
+/*
+holidayApi.countries()
+  .then((countries) => { console.log(countries); })
+  .catch((err) => { console.error(err); });
+
+// Fetch supported languages
+holidayApi.languages()
+  .then((languages) => { console.log(languages); })
+  .catch((err) => { console.error(err); });
+
+// Fetch holidays with minimum parameters
+holidayApi.holidays({ country: 'JP', year: 2019 })
+  .then((holidays) => { console.log(holidays); })
+  .catch((err) => { console.error(err); });
+*/
+// Async? Await? No problem!
+/*
+(async () => {
+  // Fetch supported countries and subdivisions
+  const countries = await holidayApi.countries();
+
+  // Fetch supported languages
+  const languages = await holidayApi.languages();
+
+  // Fetch holidays with minimum parameters
+  const holidays = await holidayApi.holidays({
+    country: 'JP',
+    year: 2019,
+  });
+
+  console.log(holidays);
+})();
+*/
+//holiday
 
 const Sample01 = () => (
     <div style={{ height: 500}}>
@@ -30,7 +72,7 @@ const Sample01 = () => (
                 }
             ]}
             step={60}
-            view="month"//current view of the month
+            //view="week"//current view of the month
             //views={["month","week","work_week","day"]}
             views={[allViews[0], allViews[1],allViews[3]]}            
             min={new Date(2008, 0, 1, 8, 0)}//minimum time of the day and week views
